1ec6348df116e084a9fad4d30e9b4ae0722c233a
Joey Schindler <111151454+jschindler11@users.noreply.github.com>
2022-10-05 12:15:28 -0700

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -29,3 +29,21 @@ FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
 
+-- employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
